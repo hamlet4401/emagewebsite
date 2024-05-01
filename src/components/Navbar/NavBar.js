eb5be78884bff0fe3335cdc0b210ebf97a7c461c
Hamlet <hamlet.marekhashvili@hotmail.com>
2024-05-01 16:34:33 +0200

    feat: add e-mage logo

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import NavLinks from "../Navbar/NavLinks";
 import { HashLink } from "react-router-hash-link";
+import { default as EmageLogo } from "../../images/EmageLogo";
 
 const NavBar = () => {
   const [top, setTop] = useState(!window.scrollY);
@@ -26,16 +27,8 @@ const NavBar = () => {
       <div className="flex flex-row justify-between items-center py-2">
         <div className="flex flex-row justify-center md:px-12 md:mx-12 items-center text-center font-semibold">
           <HashLink smooth to="/#hero">
-            <div className="text-blue-900 mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="72"
-                height="72"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path>
-              </svg>
+            <div className="text-blue-900 w-72 h-16">
+              <EmageLogo></EmageLogo>
             </div>
           </HashLink>
         </div>
---
+++ src/images/EmageLogo.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+const EmageLogo = () => {
+  return (
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="988,450,1805.50382,700"
+      fill="currentColor"
+      stroke="currentColor"
+      className="size-full"
+    >
+      <g class="layer">
+        <title>Layer 1</title>
+        <g fill-rule="nonzero" id="document" />
+        <g fill-rule="nonzero" id="svg_5">
+          <g id="stage">
+            <path
+              d="m1236.61,528.16c0.86,-19.62 3.64,-40.05 -9.93,-59.06c14.62,4.06 28.8,9.27 42.48,16.82"
+              fill="currentColor"
+              fill-opacity="0"
+              font-family="Sans-serif"
+              id="tip"
+              stroke="currentColor"
+              stroke-dasharray="0,0"
+              stroke-linecap="round"
+              stroke-width="15"
+            />
+            <path
+              d="m1271.2,487.48c32.13,60.3 64.91,120.1 92.98,182.47c23.79,52.87 44.09,107.74 63.01,162.51"
+              fill="currentColor"
+              fill-opacity="0"
+              font-family="Sans-serif"
+              id="front_vertical"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-width="20"
+            />
+            <path
+              d="m1009.59,736.8c15.82,-13.01 32.65,-24.48 51.74,-32.2c56.04,-22.67 122.5,-10.62 176.75,11.82c25.18,10.41 49.11,23.63 74.19,34.29c59.02,25.09 122.93,47.06 187.71,46.49c6.39,-0.16 12.77,-0.45 19.14,-1.13c10.61,-1.13 35.38,-5.58 27.24,-22.56c-1.47,-3.07 -3.39,-5.92 -5.47,-8.61c-1.57,-2.01 -6.76,-6.92 -9.43,-8.63c-18.16,-11.64 -40.67,-15.02 -61.85,-15.5"
+              fill="currentColor"
+              fill-opacity="0"
+              font-family="Sans-serif"
+              id="horizontal_cap"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="30"
+            />
+            <path
+              d="m1182.98,678.16c14.63,-12.02 28.42,-25.17 39.22,-40.82c23.59,-34.2 33.34,-77.35 34.1,-118.37"
+              fill="currentColor"
+              fill-opacity="0"
+              font-family="Sans-serif"
+              id="back_vertical"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="15"
+            />
+            <path
+              d="m1039.28,752.69c19.31,-13.75 43.78,-26.48 71.34,-29.88c30.83,-3.8 64.35,4.39 91.27,13.06"
+              fill="currentColor"
+              fill-opacity="0"
+              font-family="Sans-serif"
+              id="horizontal_extra"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="5"
+            />
+            <text
+              fill="currentColor"
+              font-family="Times"
+              font-size="40"
+              id="emage"
+              stroke="currentColor"
+              text-anchor="middle"
+              transform="matrix(7.71961 0 0 6.91667 0 0)"
+              x="264.49"
+              y="105.52"
+            >
+              e-mage
+            </text>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default EmageLogo;
