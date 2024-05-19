dc5d1a6e5b096ae14a84033d3e8e04f479e14eca
Hamlet <hamlet.marekhashvili@hotmail.com>
2024-05-19 14:23:33 +0200

    fix: fix e-mail layout

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -67,7 +67,7 @@ const EmailForm = () => {
       });
   };
   return (
-    <form onSubmit={sendEmail}>
+    <form onSubmit={sendEmail} className="w-full">
       <div className="w-full bg-white p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
         <div className="flex">
           <h1 className="font-bold text-center lg:text-left text-blue-900 uppercase text-4xl">
