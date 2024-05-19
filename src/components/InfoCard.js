771d452ac2cd63b175ef6c37cab31bcd07cd9370
Hamlet <hamlet.marekhashvili@hotmail.com>
2024-05-19 14:23:06 +0200

    refactor: update infocard layout

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -6,16 +6,20 @@ const InfoCard = ({ mainCard, extraCard, mirrored = false }) => {
       <div className="container mx-auto my-8 px-4 lg:px-20" data-aos="zoom-in">
         {mirrored ? (
           <>
-            <div className="w-full lg:ml-96 pr-8">{mainCard}</div>
-            <div className="w-full lg:-mt-96 lg:w-2/6 px-8 py-6 bg-blue-900 rounded-2xl">
-              <div className="flex flex-col text-white">{extraCard}</div>
+            <div className="w-full flex justify-end">{mainCard}</div>
+            <div className="w-full flex justify-start">
+              <div className="w-4/6 xl:-mt-96 -mt-20 xl:w-2/6 px-8 py-6 bg-blue-900 rounded-2xl">
+                <div className="flex flex-col text-white">{extraCard}</div>
+              </div>
             </div>
           </>
         ) : (
           <>
-            <div className="w-full">{mainCard}</div>
-            <div className="w-full lg:-mt-96 lg:w-2/6 px-8 py-6 ml-auto bg-blue-900 rounded-2xl">
-              <div className="flex flex-col text-white">{extraCard}</div>
+            <div className="w-full flex justify-start">{mainCard}</div>
+            <div className="w-full flex justify-end">
+              <div className="w-4/6 xl:-mt-96 -mt-20 xl:w-2/6 px-8 py-6 bg-blue-900 rounded-2xl">
+                <div className="flex flex-col text-white">{extraCard}</div>
+              </div>
             </div>
           </>
         )}
