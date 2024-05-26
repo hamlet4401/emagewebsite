066e1d81164fa65c429297a143e7edf41612b142
Hamlet <hamlet.marekhashvili@hotmail.com>
2024-05-26 17:55:40 +0200

    feat: removed social media links for now

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -45,7 +45,7 @@ const Contact = () => {
                 </div>
               </div>
 
-              <div className="flex my-4 w-2/3 lg:w-1/2">
+              {/* <div className="flex my-4 w-2/3 lg:w-1/2">
                 <a
                   href="https://www.linkedin.com/company/e-magecommv"
                   target="_blank"
@@ -79,7 +79,7 @@ const Contact = () => {
                     <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
                   </svg>
                 </a>
-              </div>
+              </div> */}
             </>
           }
         />
